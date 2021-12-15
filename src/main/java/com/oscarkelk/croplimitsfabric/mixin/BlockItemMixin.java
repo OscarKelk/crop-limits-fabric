899a26ca312e01b00d382f51f17250c58401f5a3
OscarKelk <oscar@oscarkelk.com>
2021-12-15 17:35:45 +1100

    Mod Works

--- src/main/java/com/oscarkelk/croplimitsfabric/mixin/BlockItemMixin.java
+++ src/main/java/com/oscarkelk/croplimitsfabric/mixin/BlockItemMixin.java
@@ -1,68 +1,89 @@
 package com.oscarkelk.croplimitsfabric.mixin;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.biome.Biome.Category;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-
-import java.util.Optional;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockItem.class)
-public abstract class BlockItemMixin extends Entity {
-
-    public BlockItemMixin(EntityType<?> type, World world) {
-        super(type, world);
-    }
-
-    @Inject(method = "place", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/BlockItem;getBlockState()V"))
-    private ActionResult place(ItemPlacementContext context) {
+public class BlockItemMixin {
+//    @Inject(method = "place", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/BlockItem;getBlockState()V"))
+    @Inject(method = "place", at = @At(value = "HEAD"), cancellable = true)
+    private void place(ItemPlacementContext context, CallbackInfoReturnable<ActionResult> ci) {
+        Item item = context.getStack().getItem();
         Biome biome = context.getPlayer().getWorld().getBiome(context.getBlockPos());
-        Optional<RegistryKey<Biome>> biomeKey = context.getPlayer().getWorld().getBiomeKey(context.getBlockPos());
+        Category biomeCategory = context.getWorld().getBiomeAccess().getBiome(context.getBlockPos()).getCategory();
         RegistryKey<World> registryKey = context.getPlayer().getEntityWorld().getRegistryKey();
 
-        // Hot biomes
-        if (biome.getTemperature() >= 1.0f) {
-            if (context.getStack().getItem() == Items.CACTUS || context.getStack().getItem() == Items.BEETROOT_SEEDS || context.getStack().getItem() == Items.POTATO) {
-                return ActionResult.SUCCESS;
+        if (item == Items.CACTUS) {
+            if (biome.isHot(context.getBlockPos())) { // Hot biomes
+                return;
             }
+            ci.setReturnValue(ActionResult.FAIL);
         }
-        // Regular biomes
-        if (biome.getTemperature() >= 0.15f && biome.getTemperature() <= 1.0f) {
-            if (context.getStack().getItem() == Items.WHEAT_SEEDS || context.getStack().getItem() == Items.POTATO || context.getStack().getItem() == Items.PUMPKIN_SEEDS || context.getStack().getItem() == Items.SUGAR_CANE || context.getStack().getItem() == Items.MELON_SEEDS || context.getStack().getItem() == Items.SWEET_BERRIES) {
-                return ActionResult.SUCCESS;
+        if (item == Items.BEETROOT_SEEDS) {
+            if (biome.isHot(context.getBlockPos())) { // Hot biomes
+                return;
             }
+            ci.setReturnValue(ActionResult.FAIL);
         }
-        // Cold biomes
-        if (biome.getTemperature() <= 0.15f) {
-            if (context.getStack().getItem() == Items.POTATO || context.getStack().getItem() == Items.PUMPKIN_SEEDS || context.getStack().getItem() == Items.SWEET_BERRIES) {
-                return ActionResult.SUCCESS;
+        if (item == Items.WHEAT_SEEDS) {
+            if (!biome.isCold(context.getBlockPos()) && !biome.isHot(context.getBlockPos())) { // Regular biomes
+                return;
             }
+            ci.setReturnValue(ActionResult.FAIL);
         }
-        // Jungle biomes
-        if (biomeKey.equals(BiomeKeys.JUNGLE) || biomeKey.equals(BiomeKeys.BAMBOO_JUNGLE) || biomeKey.equals(BiomeKeys.SPARSE_JUNGLE)) {
-            if (context.getStack().getItem() == Items.BAMBOO || context.getStack().getItem() == Items.COCOA_BEANS || context.getStack().getItem() == Items.POTATO || context.getStack().getItem() == Items.SUGAR_CANE || context.getStack().getItem() == Items.MELON_SEEDS) {
-                return ActionResult.SUCCESS;
+        if (item == Items.PUMPKIN_SEEDS) {
+            if (!biome.isHot(context.getBlockPos())) { // Regular and cold biomes
+                return;
             }
+            ci.setReturnValue(ActionResult.FAIL);
         }
-        // Nether Dimension
-        if (registryKey == World.NETHER) {
-            if (context.getStack().getItem() != Items.NETHER_WART) {
-                return ActionResult.SUCCESS;
+        if (item == Items.SUGAR_CANE) {
+            if (!biome.isCold(context.getBlockPos()) && !biome.isHot(context.getBlockPos()) || biomeCategory.equals(Category.JUNGLE)) { // Regular biomes and jungles
+                return;
             }
+            ci.setReturnValue(ActionResult.FAIL);
         }
-        else {
-            return ActionResult.FAIL;
+        if (item == Items.MELON_SEEDS) {
+            if (!biome.isCold(context.getBlockPos()) && !biome.isHot(context.getBlockPos()) || biomeCategory.equals(Category.JUNGLE)) { // Regular biomes and jungles
+                return;
+            }
+            ci.setReturnValue(ActionResult.FAIL);
         }
-        return ActionResult.SUCCESS;
+        if (item == Items.SWEET_BERRIES) {
+            if (!biome.isHot(context.getBlockPos())) { // Regular and cold biomes
+                return;
+            }
+            ci.setReturnValue(ActionResult.FAIL);
+        }
+        if (item == Items.BAMBOO) {
+            if (biomeCategory.equals(Category.JUNGLE)) {
+                return;
+            }
+            ci.setReturnValue(ActionResult.FAIL);
+        }
+        if (item == Items.COCOA_BEANS) {
+            if (biomeCategory.equals(Category.JUNGLE)) {
+                return;
+            }
+            ci.setReturnValue(ActionResult.FAIL);
+        }
+        if (item == Items.NETHER_WART) {
+            if (registryKey == World.NETHER) {
+                return;
+            }
+            ci.setReturnValue(ActionResult.FAIL);
+        }
     }
 }
 
